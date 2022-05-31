c4bdede43e4f0166edc8d1cb45ef8769570d7745
Duchez <edukoo@gmail.com>
2022-05-31 20:18:35 +0300

    prints formatted 0 - 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i == 99:
-        print("{}".format(i), end="\n")
-    elif i < 10:
-        print("0{}, ".format(i), end="")
+        print("{}".format(i))
     else:
-        print("{}, ".format(i), end="")
+        print("{:02}".format(i), end=", ")
