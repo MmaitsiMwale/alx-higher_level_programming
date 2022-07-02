64920492d4fb8be8423865ede954a5934690948e
MmaitsiMwale <mmaitsimwale@gmail.com>
2022-07-03 00:47:02 +0300

    prints a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 0:
+        print("$")
+    for i in matrix:
+        for j, el in enumerate(i):
+            if j == len(i) - 1:
+                print("{:d}$".format(el), end="\n")
+            else:
+                print("{:d} ".format(el), end="")
